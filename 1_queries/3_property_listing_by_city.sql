c960bf8dc94d32228780cee517d5f39f61953e05
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-26 19:27:42 -0400

    1_queries

---
+++ 1_queries/3_property_listing_by_city.sql
@@ -0,0 +1,18 @@
+SELECT
+  properties.id,
+  properties.title,
+  properties.cost_per_night,
+  avg(property_reviews.rating) as avg_rating
+FROM
+  properties
+  join property_reviews on properties.id = property_reviews.property_id
+WHERE
+  city = 'ancouver'
+GROUP BY
+  properties.id
+HAVING
+  avg(property_reviews.rating) >= 4
+ORDER BY
+  avg_rating
+LIMIT
+  10;
---
+++ 1_queries/4_most_visited_cities.sql
@@ -0,0 +1,13 @@
+SELECT
+  COUNT(*) as total from (
+    SELECT
+      properties.city,
+      count(reservations.*) as total_reservations
+    FROM
+      properties
+      JOIN reservations ON properties.id = reservations.property_id
+    GROUP BY
+      properties.city
+    ORDER BY
+      total_reservations DESC
+  ) AS suquery;
